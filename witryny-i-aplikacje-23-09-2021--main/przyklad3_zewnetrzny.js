2dd22f94fe855f99191bd6f8ed5ea3f294b479e2
yeet13574 <95480260+yeet13574@users.noreply.github.com>
2021-12-03 13:48:52 +0100

    Add files via upload

---
+++ witryny-i-aplikacje-23-09-2021--main/przyklad3_zewnetrzny.js
@@ -0,0 +1,6 @@
+document.write("ostatnia modyfikacja strony".fontcolor("red").bold().fontsize(7)+"<br>");
+document.write(document.lastModified);
+function okno_zamknij_radchenko_kod()
+{
+window.close()
+}
---
+++ witryny-i-aplikacje-23-09-2021--main/script.js
@@ -0,0 +1,281 @@
+$(document).ready(function(){
+    $('.parallax_lista>li').addClass('layer');
+    $('.parallax_lista').parallax();
+
+    $('.glowna').addClass('active');
+});
+function zadanie1()
+{
+window.open("przyklad1a.html");
+}
+
+function zadanie1_k()
+{
+window.open("przyklad1a_kod.html");
+}
+
+function zadanie2_k()
+{
+window.open("zadanie2_kod.html");
+}
+
+function zadanie2()
+{
+window.open("zadanie2.html");
+}
+
+function zadanie3()
+{
+window.open("zadanie3.html");
+}
+
+function zadanie4()
+{
+window.open("zadanie4.html");
+}
+
+function zadanie5()
+{
+window.open("zadanie5.html");
+}
+
+function zadanie6()
+{
+window.open("zadanie6.html");
+}
+
+function zadanie7()
+{
+window.open("zadanie7.html");
+}
+
+function zadanie8()
+{
+window.open("zadanie8.html");
+}
+
+function zadanie8_k()
+{
+window.open("zadanie8_kod.html");
+}
+
+function zadanie9()
+{
+window.open("zadanie9.html");
+}
+
+function zadanie9_k()
+{
+window.open("zadanie9_kod.html");
+}
+
+function zadanie10()
+{
+window.open("zadanie10.html");
+}
+
+function zadanie10_k()
+{
+window.open("zadanie10_kod.html");
+}
+
+function zadanie11()
+{
+window.open("zadanie11.html");
+}
+
+function zadanie11_k()
+{
+window.open("zadanie11_kod.html");
+}
+
+function zadanie12()
+{
+window.open("zadanie12.html");
+}
+
+function zadanie12_k()
+{
+window.open("zadanie12_kod.html");
+}
+
+function zadanie13()
+{
+window.open("zadanie13.html");
+}
+
+function zadanie13_k()
+{
+window.open("zadanie13_kod.html");
+}
+
+function zadanie14()
+{
+window.open("zadanie14.html");
+}
+
+function zadanie14_k()
+{
+window.open("zadanie14_kod.html");
+}
+
+function zadanie15()
+{
+window.open("zadanie15.html");
+}
+
+function zadanie15_k()
+{
+window.open("zadanie15_kod.html");
+}
+
+function zadanie16()
+{
+window.open("zadanie16.html");
+}
+
+function zadanie17()
+{
+window.open("zadanie17.html");
+}
+
+function zadanie17_k()
+{
+window.open("zadanie17_kod.html");
+}
+
+function zadanie18()
+{
+window.open("zadanie18.html");
+}
+
+function zadanie19()
+{
+window.open("zadanie19.html");
+}
+
+function zadanie19_k()
+{
+window.open("zadanie19_kod.html");
+}
+
+function zadanie20()
+{
+window.open("zadanie20.html");
+}
+
+function zadanie20_k()
+{
+window.open("zadanie20_kod.html");
+}
+
+function zadanie21()
+{
+window.open("zadanie21.html");
+}
+function zadanie21_k()
+{
+window.open("zadanie21_kod.html");
+}
+
+function zadanie22()
+{
+window.open("zadanie22.html");
+}
+
+function zadanie22_k()
+{
+window.open("zadanie22_kod.html");
+}
+
+function zadanie23()
+{
+window.open("zadanie23.html");
+}
+
+function zadanie23_k()
+{
+window.open("zadanie23_kod.html");
+}
+
+function zadanie23_kjs()
+{
+window.open("zadanie23_kod_javascript.html");
+}
+
+function zadanie62()
+{
+window.open("zadanie62.html");
+}
+function zadanie62_k()
+{
+window.open("zadanie62_kod.html");
+}
+
+function zadanie63()
+{
+window.open("zadanie63.html");
+}
+function zadanie63_k()
+{
+window.open("zadanie63_kod.html");
+}
+
+function zadanie64()
+{
+window.open("zadanie64.html");
+}
+function zadanie64_k()
+{
+window.open("zadanie64_kod.html");
+}
+
+function zadanie65()
+{
+window.open("zadanie65.html");
+}
+function zadanie65_k()
+{
+window.open("zadanie65_kod.html");
+}
+
+function zadanie66()
+{
+window.open("zadanie66.html");
+}
+function zadanie66_k()
+{
+window.open("zadanie66_kod.html");
+}
+
+function zadanie67()
+{
+window.open("zadanie67.html");
+}
+function zadanie67_k()
+{
+window.open("zadanie67_kod.html");
+}
+
+function zadanie68()
+{
+window.open("zadanie68.html");
+}
+function zadanie68_k()
+{
+window.open("zadanie68_kod.html");
+}
+
+function zadanie69()
+{
+window.open("zadanie69.html");
+}
+function zadanie69_k()
+{
+window.open("zadanie69_kod.html");
+}
+
+function zadanie70()
+{
+window.open("zadanie70.html");
+}
